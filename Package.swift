416b48a01acbafc4d17e8c01583127b8d16363f2
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2026-08-10 15:46:27 -0700

    Adopt the Organization Repository Standards

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:6.2
 
 //
-// This source file is part of the ENGAGE-HF-AI-Voice open source project
+// This source file is part of the ENGAGE-HF AI-Voice open-source project
 // 
 // SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
 // 
